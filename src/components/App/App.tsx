5044e1880dcaad1af042ea18050e4a97794aa9d5
HerveDelaunay <delaunay.herve06@gmail.com>
2022-08-15 19:42:51 +0200

    setup done

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="font-sans">Test numero 1</div>
+      <div className="font-serif">Test numero 2</div>
+    </>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,tsx}"],
+  theme: {
+    colors: {
+      'cyan': '#5FB4A2',
+      'blue': '#203A4C',
+      'dark-blue': '#33323D',
+      'light-grey': '#FAFAFA',
+      'grey': '#EAEAEB',
+      'red': '#F43030',
+    },
+    fontFamily: {
+      sans: ['Public Sans', 'sans-serif'],
+      serif: ['Ibarra Real Nova', 'serif'],
+    },
+  },
+  plugins: [],
+}
