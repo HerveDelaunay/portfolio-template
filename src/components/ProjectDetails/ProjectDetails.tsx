3f57c9c7bb30cc22d3403f64cbee81d5ece5dc90
HerveDelaunay <delaunay.herve06@gmail.com>
2022-08-27 01:14:19 +0200

    feat-tablet: project details page layout wip

--- src/components/ProjectDetails/ProjectDetails.tsx
+++ src/components/ProjectDetails/ProjectDetails.tsx
@@ -14,6 +14,18 @@ import insurePreview1 from "../../assets/images/detail/mobile/image-insure-previ
 import insurePreview2 from "../../assets/images/detail/mobile/image-insure-preview-2.jpg";
 import managePreview1 from "../../assets/images/detail/mobile/image-manage-preview-1.jpg";
 import managePreview2 from "../../assets/images/detail/mobile/image-manage-preview-2.jpg";
+import bookmarkHeroTablet from "../../assets/images/detail/mobile/image-bookmark-hero.jpg";
+import fyloHeroTablet from "../../assets/images/detail/tablet/image-fylo-hero.jpg";
+import insureHeroTablet from "../../assets/images/detail/tablet/image-insure-hero.jpg";
+import manageHeroTablet from "../../assets/images/detail/tablet/image-manage-hero.jpg";
+import bookmarkPreview1Tablet from "../../assets/images/detail/tablet/image-bookmark-preview-1.jpg";
+import bookmarkPreview2Tablet from "../../assets/images/detail/tablet/image-bookmark-preview-2.jpg";
+import fyloPreview1Tablet from "../../assets/images/detail/tablet/image-fylo-preview-1.jpg";
+import fyloPreview2Tablet from "../../assets/images/detail/tablet/image-fylo-preview-2.jpg";
+import insurePreview1Tablet from "../../assets/images/detail/tablet/image-insure-preview-1.jpg";
+import insurePreview2Tablet from "../../assets/images/detail/tablet/image-insure-preview-2.jpg";
+import managePreview1Tablet from "../../assets/images/detail/tablet/image-manage-preview-1.jpg";
+import managePreview2Tablet from "../../assets/images/detail/tablet/image-manage-preview-2.jpg";
 import arrowLeft from "../../assets/images/icons/arrow-left.svg";
 import arrowRight from "../../assets/images/icons/arrow-right.svg";
 import { useEffect } from "react";
@@ -25,6 +37,12 @@ const ProjectDetails: React.FC<Props> = ({
   setProjectIndex,
 }) => {
   const heroImages = [manageHero, bookmarkHero, insureHero, fyloHero];
+  const heroImagesTablet = [
+    manageHeroTablet,
+    bookmarkHeroTablet,
+    insureHeroTablet,
+    fyloHeroTablet,
+  ];
   const preview1 = [
     managePreview1,
     bookmarkPreview1,
@@ -37,6 +55,18 @@ const ProjectDetails: React.FC<Props> = ({
     insurePreview2,
     fyloPreview2,
   ];
+  const preview1Tablet = [
+    managePreview1Tablet,
+    bookmarkPreview1Tablet,
+    insurePreview1Tablet,
+    fyloPreview1Tablet,
+  ];
+  const preview2Tablet = [
+    managePreview2Tablet,
+    bookmarkPreview2Tablet,
+    insurePreview2Tablet,
+    fyloPreview2Tablet,
+  ];
   const { slug } = useParams();
   useEffect(() => {
     const slugToIndex = projectIndexData.findIndex(
@@ -46,27 +76,37 @@ const ProjectDetails: React.FC<Props> = ({
   });
   return (
     <>
-      <main className="flex flex-col items-center w-10/12">
+      <main className="flex flex-col items-center w-10/12 tablet:w-[689px] tablet:mt-[94px]">
         <article className="w-full mb-12">
           <img
             src={heroImages[projectIndex]}
             alt="image-project"
-            className="w-full"
+            className="w-full tablet:hidden"
           />
-          <div className="w-full h-[28.25rem] border-t border-b border-opacity mt-8 flex flex-col justify-evenly">
-            <h2 className="text-h2 text-dark-blue font-bold font-serif">
-              {data[projectIndex]["project-name"]}
-            </h2>
-            <p className="text-p2 text text-dark-blue opacity-p">
+          <img
+            src={heroImagesTablet[projectIndex]}
+            alt="image-project"
+            className="w-full max-tablet:hidden tablet:h-[310px]"
+          />
+          <div className="w-full h-[28.25rem] border-t border-b border-opacity mt-8 tablet:mt-10 tablet:flex tablet:flex-row tablet:items-center tablet:justify-between tablet:h-[248px]">
+            <div className="w-full h-full flex flex-col justify-evenly tablet:w-[338px] tablet:h-[182px] tablet:flex-col tablet:justify-between">
+              <h2 className="text-h2 text-dark-blue font-bold font-serif">
+                {data[projectIndex]["project-name"]}
+              </h2>
+              <p className="text-p2 text text-dark-blue opacity-p tablet:hidden">
+                {data[projectIndex]["project-description"]}
+              </p>
+              <p className="text-techno text-cyan">
+                Interaction Design / Front End Development <br />
+                HTML / CSS / JS
+              </p>
+              <button className="border w-btn h-12 btn-text-black">
+                VISIT WEBSITE
+              </button>
+            </div>
+            <p className="text-p2 text text-dark-blue opacity-p max-tablet:hidden w-[339px] tablet:h-[182px]">
               {data[projectIndex]["project-description"]}
             </p>
-            <p className="text-techno text-cyan">
-              Interaction Design / Front End Development <br />
-              HTML / CSS / JS
-            </p>
-            <button className="border w-btn h-12 btn-text-black">
-              VISIT WEBSITE
-            </button>
           </div>
         </article>
         <article className="w-full mb-[2.5rem] h-[25rem] flex flex-col justify-between">
