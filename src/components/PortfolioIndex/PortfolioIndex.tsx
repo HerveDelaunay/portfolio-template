dcdc5dc8b26d5734890a9f976a5bca53aac548d9
HerveDelaunay <delaunay.herve06@gmail.com>
2022-08-29 17:39:11 +0200

    feat-desktop: project details page layout done

--- src/components/PortfolioIndex/PortfolioIndex.tsx
+++ src/components/PortfolioIndex/PortfolioIndex.tsx
@@ -1,16 +1,19 @@
-import data from "../../assets/portfolio-index.json";
+import { Link } from "react-router-dom";
 import contactData from "../../assets/data.json";
+import bookmarkImageDesktop from "../../assets/images/portfolio/desktop/image-portfolio-bookmark@2x.jpg";
+import fyloImageDesktop from "../../assets/images/portfolio/desktop/image-portfolio-fylo@2x.jpg";
+import insureImageDesktop from "../../assets/images/portfolio/desktop/image-portfolio-insure@2x.jpg";
+import manageImageDesktop from "../../assets/images/portfolio/desktop/image-portfolio-manage@2x.jpg";
 import bookmarkImage from "../../assets/images/portfolio/mobile/image-portfolio-bookmark.jpg";
-import manageImage from "../../assets/images/portfolio/mobile/image-portfolio-manage.jpg";
 import fyloImage from "../../assets/images/portfolio/mobile/image-portfolio-fylo.jpg";
 import insureImage from "../../assets/images/portfolio/mobile/image-portfolio-insure.jpg";
+import manageImage from "../../assets/images/portfolio/mobile/image-portfolio-manage.jpg";
 import bookmarkImageTablet from "../../assets/images/portfolio/tablet/image-portfolio-bookmark.jpg";
-import manageImageTablet from "../../assets/images/portfolio/tablet/image-portfolio-manage.jpg";
 import fyloImageTablet from "../../assets/images/portfolio/tablet/image-portfolio-fylo.jpg";
 import insureImageTablet from "../../assets/images/portfolio/tablet/image-portfolio-insure.jpg";
+import manageImageTablet from "../../assets/images/portfolio/tablet/image-portfolio-manage.jpg";
+import data from "../../assets/portfolio-index.json";
 import Props from "./Type";
-import { Link } from "react-router-dom";
-import { useEffect } from "react";
 
 const PortfolioIndex: React.FC<Props> = ({ setProjects, setProjectIndex }) => {
   const images = [manageImage, bookmarkImage, insureImage, fyloImage];
@@ -20,6 +23,12 @@ const PortfolioIndex: React.FC<Props> = ({ setProjects, setProjectIndex }) => {
     insureImageTablet,
     fyloImageTablet,
   ];
+  const imagesDesktop = [
+    manageImageDesktop,
+    bookmarkImageDesktop,
+    insureImageDesktop,
+    fyloImageDesktop,
+  ];
   const handleClickProject = (index: number) => {
     setProjects([...images]);
     setProjectIndex(index);
@@ -27,10 +36,15 @@ const PortfolioIndex: React.FC<Props> = ({ setProjects, setProjectIndex }) => {
 
   return (
     <>
-      <main className="flex flex-col items-center w-10/12 tablet:w-[689px] tablet:h-[1912px] tablet:mt-[94px] tablet:justify-between">
+      <main
+        className="flex flex-col items-center w-10/12 tablet:w-[689px] tablet:h-[1912px] tablet:mt-[94px] tablet:justify-between 
+        desktop:w-[1111px] desktop:h-[2240px] desktop:justify-between"
+      >
         {data.map((project, index) => (
           <article
-            className="w-full mb-[4.5rem] tablet:h-[418px] tablet:mb-0 tablet:flex tablet:flex-row tablet:items-center tablet:justify-between"
+            className={`w-full mb-[4.5rem] tablet:h-[418px] tablet:mb-0 tablet:flex tablet:flex-row tablet:items-center 
+              tablet:justify-between desktop:w-[1015px] desktop:h-[500px]
+              ${index % 2 === 0 ? "desktop:self-start" : "desktop:self-end"}`}
             key={index}
           >
             <img
@@ -41,20 +55,30 @@ const PortfolioIndex: React.FC<Props> = ({ setProjects, setProjectIndex }) => {
             <img
               src={imagesTablet[index]}
               alt="image-profile-tablet"
-              className={`w-[339px] max-tablet:hidden h-[314px] ${
+              className={`w-[339px] max-tablet:hidden desktop:hidden h-[314px] ${
                 index % 2 === 0 ? "order-first" : "order-last"
               }`}
             />
-            <div className="w-full h-[23rem] border-y border-opacity mt-8 flex flex-col max-tablet:justify-evenly tablet:w-[281px] tablet:h-[418px] tablet:mt-0">
-              <h2 className="text-h2 text-dark-blue font-bold font-serif tablet:mt-8">
+            <img
+              src={imagesDesktop[index]}
+              alt="image-profile-tablet"
+              className={`w-[540px] max-desktop:hidden h-[500px] ${
+                index % 2 === 0 ? "order-first" : "order-last"
+              }`}
+            />
+            <div
+              className="w-full h-[23rem] border-y border-opacity mt-8 flex flex-col max-tablet:justify-evenly tablet:w-[281px] tablet:h-[418px] tablet:mt-0
+              desktop:h-full desktop:w-[350px] desktop:justify-center"
+            >
+              <h2 className="text-h2 text-dark-blue font-bold font-serif tablet:mt-8 desktop:mt-0">
                 {project["project-name"]}
               </h2>
-              <p className="text-p2 text-dark-blue opacity-p tablet:mt-[29px]">
+              <p className="text-p2 text-dark-blue opacity-p tablet:mt-[29px] desktop:mt-7">
                 {project["project-description"]}
               </p>
               <Link to={project["project-name"]}>
                 <button
-                  className="border w-btn h-12 btn-text-black tablet:mt-[35px]"
+                  className="border w-btn h-12 btn-text-black tablet:mt-[35px] desktop:mt-6"
                   onClick={() => handleClickProject(index)}
                 >
                   VIEW PROJECT
@@ -64,12 +88,18 @@ const PortfolioIndex: React.FC<Props> = ({ setProjects, setProjectIndex }) => {
           </article>
         ))}
       </main>
-      <div className="w-full h-[25.5625rem] flex justify-center tablet:w-[689px] tablet:h-[84px] tablet:my-[96px]">
-        <section className="w-10/12 h-[13.375rem] flex flex-col justify-between items-center mt-[7.1875rem] tablet:mt-0 tablet:flex-row tablet:items-center tablet:w-full tablet:h-[84px]">
+      <div
+        className="w-full h-[25.5625rem] flex justify-center tablet:w-[689px] tablet:h-[84px] tablet:my-[96px] 
+        desktop:my-[150px] desktop:w-[1110px]"
+      >
+        <section
+          className="w-10/12 h-[13.375rem] flex flex-col justify-between items-center mt-[7.1875rem] tablet:mt-0 
+          tablet:flex-row tablet:items-center tablet:w-full tablet:h-[84px] desktop:w-full"
+        >
           <h2 className="text-h2 font-bold font-serif text-center h-[7.875rem] text-dark-blue tablet:w-[350px] tablet:h-[84px]">
             {contactData["contact me"]}
           </h2>
-          <span className="max-tablet:hidden w-[113px] h-[1px] bg-dark-blue/15"></span>
+          <span className="max-tablet:hidden w-[113px] h-[1px] bg-dark-blue/15 desktop:w-[534px]"></span>
           <Link to="/contact-me">
             <button className="w-[10.125rem] h-12 border text-btn text-dark-blue">
               CONTACT ME
--- src/components/ProjectDetails/ProjectDetails.tsx
+++ src/components/ProjectDetails/ProjectDetails.tsx
@@ -26,6 +26,18 @@ import insurePreview1Tablet from "../../assets/images/detail/tablet/image-insure
 import insurePreview2Tablet from "../../assets/images/detail/tablet/image-insure-preview-2@2x.jpg";
 import managePreview1Tablet from "../../assets/images/detail/tablet/image-manage-preview-1@2x.jpg";
 import managePreview2Tablet from "../../assets/images/detail/tablet/image-manage-preview-2@2x.jpg";
+import bookmarkHeroDesktop from "../../assets/images/detail/desktop/image-bookmark-hero@2x.jpg";
+import fyloHeroDesktop from "../../assets/images/detail/desktop/image-fylo-hero@2x.jpg";
+import insureHeroDesktop from "../../assets/images/detail/desktop/image-insure-hero@2x.jpg";
+import manageHeroDesktop from "../../assets/images/detail/desktop/image-manage-hero@2x.jpg";
+import bookmarkPreview1Desktop from "../../assets/images/detail/desktop/image-bookmark-preview-1@2x.jpg";
+import bookmarkPreview2Desktop from "../../assets/images/detail/desktop/image-bookmark-preview-2@2x.jpg";
+import fyloPreview1Desktop from "../../assets/images/detail/desktop/image-fylo-preview-1@2x.jpg";
+import fyloPreview2Desktop from "../../assets/images/detail/desktop/image-fylo-preview-2@2x.jpg";
+import insurePreview1Desktop from "../../assets/images/detail/desktop/image-insure-preview-1@2x.jpg";
+import insurePreview2Desktop from "../../assets/images/detail/desktop/image-insure-preview-2@2x.jpg";
+import managePreview1Desktop from "../../assets/images/detail/desktop/image-manage-preview-1@2x.jpg";
+import managePreview2Desktop from "../../assets/images/detail/desktop/image-manage-preview-2@2x.jpg";
 import arrowLeft from "../../assets/images/icons/arrow-left.svg";
 import arrowRight from "../../assets/images/icons/arrow-right.svg";
 import { useEffect } from "react";
@@ -42,6 +54,12 @@ const ProjectDetails: React.FC<Props> = ({
     bookmarkHeroTablet,
     insureHeroTablet,
     fyloHeroTablet,
+  ];
+  const heroImagesDesktop = [
+    manageHeroDesktop,
+    bookmarkHeroDesktop,
+    insureHeroDesktop,
+    fyloHeroDesktop,
   ];
   const preview1 = [
     managePreview1,
@@ -67,6 +85,18 @@ const ProjectDetails: React.FC<Props> = ({
     insurePreview2Tablet,
     fyloPreview2Tablet,
   ];
+  const preview1Desktop = [
+    managePreview1Desktop,
+    bookmarkPreview1Desktop,
+    insurePreview1Desktop,
+    fyloPreview1Desktop,
+  ];
+  const preview2Desktop = [
+    managePreview2Desktop,
+    bookmarkPreview2Desktop,
+    insurePreview2Desktop,
+    fyloPreview2Desktop,
+  ];
   const { slug } = useParams();
   useEffect(() => {
     const slugToIndex = projectIndexData.findIndex(
@@ -76,65 +106,82 @@ const ProjectDetails: React.FC<Props> = ({
   });
   return (
     <>
-      <main className="flex flex-col items-center w-10/12 tablet:w-[689px] tablet:mt-[94px]">
-        <article className="w-full mb-12 tablet:mb-10">
+      <main className="flex flex-col items-center w-10/12 tablet:w-[689px] tablet:mt-[94px] desktop:w-[1110px]">
+        <div className="w-full mb-10 tablet:mb-10 desktop:mb-[115px]">
           <img
             src={heroImages[projectIndex]}
             alt="image-project"
-            className="w-full tablet:hidden"
+            className="w-full tablet:hidden desktop:hidden"
           />
           <img
             src={heroImagesTablet[projectIndex]}
             alt="image-project"
-            className="w-full max-tablet:hidden tablet:h-[310px]"
+            className="w-full max-tablet:hidden tablet:h-[310px] desktop:hidden"
           />
-          <div className="w-full h-[28.25rem] border-t border-b border-opacity mt-8 tablet:mt-10 tablet:flex tablet:flex-row tablet:items-center tablet:justify-between tablet:h-[248px]">
-            <div className="w-full h-full flex flex-col justify-evenly tablet:w-[338px] tablet:h-[182px] tablet:flex-col tablet:justify-between">
-              <h2 className="text-h2 text-dark-blue font-bold font-serif">
-                {data[projectIndex]["project-name"]}
-              </h2>
-              <p className="text-p2 text text-dark-blue opacity-p tablet:hidden">
+          <img
+            src={heroImagesDesktop[projectIndex]}
+            alt="image-project"
+            className="w-full max-desktop:hidden h-[500px]"
+          />
+        </div>
+        <div className="flex flex-col items-center desktop:flex-row desktop:items-start desktop:justify-between desktop:w-full">
+          <article className="w-full desktop:w-[350px]">
+            <div
+              className="w-full h-[28.25rem] border-t border-b border-opacity tablet:flex tablet:flex-row tablet:items-center 
+                tablet:justify-between tablet:h-[248px] desktop:h-[498px] desktop:w-full"
+            >
+              <div
+                className="w-full h-full flex flex-col justify-evenly tablet:w-[338px] tablet:h-[182px] tablet:flex-col tablet:justify-between
+              desktop:w-full desktop:h-[400px]"
+              >
+                <h2 className="text-h2 text-dark-blue font-bold font-serif">
+                  {data[projectIndex]["project-name"]}
+                </h2>
+                <p className="text-p2 text text-dark-blue opacity-p tablet:hidden desktop:flex">
+                  {data[projectIndex]["project-description"]}
+                </p>
+                <p className="text-techno text-cyan">
+                  Interaction Design / Front End Development <br />
+                  HTML / CSS / JS
+                </p>
+                <button className="border w-btn h-12 btn-text-black">
+                  VISIT WEBSITE
+                </button>
+              </div>
+              <p className="text-p2 text text-dark-blue opacity-p max-tablet:hidden w-[339px] tablet:h-[182px] desktop:hidden">
                 {data[projectIndex]["project-description"]}
               </p>
-              <p className="text-techno text-cyan">
-                Interaction Design / Front End Development <br />
-                HTML / CSS / JS
-              </p>
-              <button className="border w-btn h-12 btn-text-black">
-                VISIT WEBSITE
-              </button>
             </div>
-            <p className="text-p2 text text-dark-blue opacity-p max-tablet:hidden w-[339px] tablet:h-[182px]">
-              {data[projectIndex]["project-description"]}
-            </p>
+          </article>
+          <div className="desktop:flex desktop:flex-col desktop:w-[635px] desktop:justify-between">
+            <article className="w-full mb-[2.5rem] h-[25rem] flex flex-col justify-between tablet:h-[220px] desktop:h-[250px] mt-12 tablet:my-10">
+              <h2 className="text-h3-mobile text-dark-blue font-serif">
+                Project Background
+              </h2>
+              <p className="text-p2 opacity-p">
+                {data[projectIndex]["project-background"]}
+              </p>
+            </article>
+            <section className="mb-[4.0625rem] tablet:w-full tablet:mb-[80px] desktop:h-[902px] desktop:mb-[64px]">
+              <h3 className="text-h3-mobile text-dark-blue font-serif mb-10 desktop:mb-7">
+                Static Previews
+              </h3>
+              <img
+                src={preview1Desktop[projectIndex]}
+                alt="first static preview of the projet"
+                className="mb-8"
+              />
+              <img
+                src={preview2Desktop[projectIndex]}
+                alt="second static preview of the projet"
+              />
+            </section>
           </div>
-        </article>
-        <article className="w-full mb-[2.5rem] h-[25rem] flex flex-col justify-between tablet:h-[220px]">
-          <h2 className="text-h3-mobile text-dark-blue font-serif">
-            Project Background
-          </h2>
-          <p className="text-p2 opacity-p">
-            {data[projectIndex]["project-background"]}
-          </p>
-        </article>
-        <section className="mb-[4.0625rem] tablet:w-full tablet:mb-[90px]">
-          <h3 className="text-h3-mobile text-dark-blue font-serif mb-10">
-            Static Previews
-          </h3>
-          <img
-            src={preview1Tablet[projectIndex]}
-            alt="first static preview of the projet"
-            className="mb-8"
-          />
-          <img
-            src={preview2Tablet[projectIndex]}
-            alt="second static preview of the projet"
-          />
-        </section>
+        </div>
       </main>
       <div
         className="flex flex-row w-[20.375rem] h-[9.125rem] border-t border-b border-dark-blue border-opacity-15 mb-16
-       tablet:w-[691px] tablet:mb-0"
+       tablet:w-[691px] tablet:mb-0 desktop:w-[1110px] desktop:h-[132px]"
       >
         <Link
           to={
@@ -178,15 +225,18 @@ const ProjectDetails: React.FC<Props> = ({
           </div>
         </Link>
       </div>
-      <div className="w-full h-[25.5625rem] flex justify-center tablet:w-[689px] tablet:h-[84px] tablet:mb-[96px] tablet:mt-[80px]">
+      <div
+        className="w-full h-[25.5625rem] flex justify-center tablet:w-[689px] tablet:h-[84px] tablet:mb-[96px] tablet:mt-[80px] 
+          desktop:mb-[150px] desktop:mt-[115px] desktop:w-[1110px]"
+      >
         <section
-          className="w-10/12 h-[13.375rem] flex flex-col justify-between items-center mt-[7.1875rem] 
-        tablet:mt-0 tablet:flex-row tablet:items-center tablet:w-full tablet:h-[84px]"
+          className="w-10/12 h-[13.375rem] flex flex-col justify-between items-center mt-[7.1875rem] tablet:mt-0 
+            tablet:flex-row tablet:items-center tablet:w-full tablet:h-[84px] desktop:w-full"
         >
           <h2 className="text-h2 font-bold font-serif text-center h-[7.875rem] text-dark-blue tablet:w-[350px] tablet:h-[84px]">
             {contactData["contact me"]}
           </h2>
-          <span className="max-tablet:hidden w-[113px] h-[1px] bg-dark-blue/15"></span>
+          <span className="max-tablet:hidden w-[113px] h-[1px] bg-dark-blue/15 desktop:w-[534px]"></span>
           <Link to="/contact-me">
             <button className="w-[10.125rem] h-12 border text-btn text-dark-blue">
               CONTACT ME
